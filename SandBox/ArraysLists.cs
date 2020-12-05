03c17ad8853c11f6f8be7669fa90bcda55b06f8e
ella_agrest <ella_agrest@hotmail.com>
2020-12-05 17:54:30 -0500

    CleanUp

--- SandBox/ArraysLists.cs
+++ SandBox/ArraysLists.cs
@@ -8,18 +8,19 @@ namespace SandBox
     {
         static void Main(string[] args)
         {
-            ///1- When you post a message on Facebook, depending on the number of people who like your post, 
-            ///Facebook displays different information.
-            ///
-            /// If no one likes your post, it doesn't display anything.
-            /// If only one person likes your post, it displays: [Friend's Name] likes your post.
-            /// If two people like your post, it displays: [Friend 1] and [Friend 2] like your post.
-            /// If more than two people like your post, it displays: [Friend 1], [Friend 2] and [Number of Other
-            /// People] others like your post.
-            /// Write a program and continuously ask the user to enter different names, until the user presses Enter
-            /// (without supplying a name). Depending on the number of names provided, display a message based on
-            /// the above pattern.
-            ///
+            /* 
+            1- When you post a message on Facebook, depending on the number of people who like your post, 
+            Facebook displays different information.           
+            If no one likes your post, it doesn't display anything.
+            If only one person likes your post, it displays: [Friend's Name] likes your post.
+            If two people like your post, it displays: [Friend 1] and [Friend 2] like your post.
+            If more than two people like your post, it displays: [Friend 1], [Friend 2] and [Number of Other
+            People] others like your post.
+            Write a program and continuously ask the user to enter different names, until the user presses Enter
+            (without supplying a name). Depending on the number of names provided, display a message based on
+            the above pattern.
+            */
+            
             var names = new List<string>();
 
             while (true)
@@ -41,13 +42,11 @@ namespace SandBox
             else
                 Console.WriteLine();
 
+             /*
+             2- Write a program and ask the user to enter their name. Use an array to reverse the name a
+             nd then store the result in a new string.Display the reversed name on the console.
+             */
 
-
-
-
-            /// 2- Write a program and ask the user to enter their name. Use an array to reverse the name a
-            /// nd then store the result in a new string.Display the reversed name on the console.
-            /// 
             Console.WriteLine("Add your name:");
             var yourName = Console.ReadLine();
 
@@ -57,14 +56,12 @@ namespace SandBox
             var reversed = new string(array);
             Console.WriteLine("reversed: " + reversed);
 
+            /*
+            3 - Write a program and ask the user to enter 5 numbers.If a number has been previously entered, 
+            display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers,
+            sort them and display the result on the console.
+            */
 
-
-
-
-            ///3 - Write a program and ask the user to enter 5 numbers.If a number has been previously entered, 
-            ///display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers,
-            ///sort them and display the result on the console.
-            ///
             var numbers1 = new List<int>();
 
             while (numbers1.Count < 5)
@@ -85,16 +82,10 @@ namespace SandBox
             foreach (var number in numbers1)
                 Console.WriteLine(number);
 
-
-
-
-
-
-
-
-            ///4- Write a program and ask the user to continuously enter a number or type "Quit" to exit. 
-            ///The list of numbers may include duplicates.Display the unique numbers that the user has entered.
-            ///
+            /*
+            4- Write a program and ask the user to continuously enter a number or type "Quit" to exit. 
+            The list of numbers may include duplicates.Display the unique numbers that the user has entered.
+            */
 
             var numbers = new List<int>();
 
@@ -120,17 +111,11 @@ namespace SandBox
             foreach (var number in uniques)
                 Console.WriteLine(number);
 
-
-
-
-
-
-
-
-
-            /// 5- Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). 
-            /// If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try;
-            /// otherwise, display the 3 smallest numbers in the list.
+             /*
+             5- Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). 
+             If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try;
+             otherwise, display the 3 smallest numbers in the list.
+             */
 
             string[] elements;
             while (true)
@@ -170,8 +155,6 @@ namespace SandBox
             Console.WriteLine("The 3 smallest numbers are: ");
             foreach (var number in smallests)
                 Console.WriteLine(number);
-
-
         }
     }
 }
